e27582490c4f1a346174e0f7d72d3f0d362e32b4
Amarasystems <amarasystem.tech@gmail.com>
2024-12-04 16:58:14 +0800

    a

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react'
 
-export default function Page() {
+export default function page() {
   return (
-    <div className="relative min-h-screen">
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className=" p-6 rounded text-[48px]">404 error</div>
-      </div>
-    </div>
-  );
+    <div>page</div>
+  )
 }
