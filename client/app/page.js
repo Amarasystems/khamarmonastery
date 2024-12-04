6ca3ffa88fed2b99f25c17bd379b7bc0e48ffa62
Amarasystems <amarasystem.tech@gmail.com>
2024-12-04 16:55:39 +0800

    fix

---
+++ client/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className=" p-6 rounded text-[48px]">404 error</div>
+      </div>
+    </div>
+  );
+}
